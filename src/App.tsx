4457e4f6a77d6a98c7552254b292d926e17fa6b6
Saujanya Tripathi <iamsaujanya.ig@gmail.com>
2024-12-24 15:36:58 +0000

    tried testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,42 @@
-import { Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from '@/components/theme-provider'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-import ScrollToTop from '@/components/ScrollToTop'
-import Home from '@/pages/Home'
-import Maps from '@/pages/Maps'
-import Weapons from '@/pages/Weapons'
-import Throwables from '@/pages/Throwables'
-import Vehicles from '@/pages/Vehicles'
-import Sensitivity from '@/pages/Sensitivity'
-import Handcams from '@/pages/Handcams'
-import PrivacyPolicy from '@/pages/PrivacyPolicy'
-import TermsOfService from '@/pages/TermsOfService'
-import PatchNotes from '@/pages/PatchNotes'
-import Search from '@/pages/Search'
+//import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@/components/theme-provider';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import ScrollToTop from '@/components/ScrollToTop';
+import Home from '@/pages/Home';
+import Maps from '@/pages/Maps';
+import Weapons from '@/pages/Weapons';
+import Throwables from '@/pages/Throwables';
+import Vehicles from '@/pages/Vehicles';
+import Sensitivity from '@/pages/Sensitivity';
+import Handcams from '@/pages/Handcams';
+import PrivacyPolicy from '@/pages/PrivacyPolicy';
+import TermsOfService from '@/pages/TermsOfService';
+import PatchNotes from '@/pages/PatchNotes';
+import Search from '@/pages/Search';
 
 const App = () => {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="min-h-screen bg-background font-sans antialiased">
-        <Navbar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/maps" element={<Maps />} />
-          <Route path="/weapons" element={<Weapons />} />
-          <Route path="/throwables" element={<Throwables />} />
-          <Route path="/vehicles" element={<Vehicles />} />
-          <Route path="/sensitivity" element={<Sensitivity />} />
-          <Route path="/handcams" element={<Handcams />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-of-service" element={<TermsOfService />} />
-          <Route path="/patch-notes" element={<PatchNotes />} />
-          <Route path="/search" element={<Search />} />
-        </Routes>
-        <Footer />
-      </div>
+    <ThemeProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/maps" element={<Maps />} />
+        <Route path="/weapons" element={<Weapons />} />
+        <Route path="/throwables" element={<Throwables />} />
+        <Route path="/vehicles" element={<Vehicles />} />
+        <Route path="/sensitivity" element={<Sensitivity />} />
+        <Route path="/handcams" element={<Handcams />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-of-service" element={<TermsOfService />} />
+        <Route path="/patch-notes" element={<PatchNotes />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+      <Footer />
+      <ScrollToTop />
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
